7f805cc50fe8357403c47c6aa8091b4565bccfee
eagledawg <scott_robertson@bellsouth.net>
2020-02-14 10:14:16 -0500

    More changes per course

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List!';
-    products: any[] = [
+    imageWidth: number =50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value:string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
             "productID": 2,
             "productName": "Garden Cart",
@@ -16,7 +34,7 @@ export class ProductListComponent {
             "description": "15 gallon rolling garden cart",
             "price": 32.99,
             "starRating": 4.2,
-            "imageUrl": "assets/images/gardem_cart.png"
+            "imageUrl": "assets/images/garden_cart.png"
         },
         {
             "productID": 5,
@@ -29,4 +47,24 @@ export class ProductListComponent {
             "imageUrl": "assets/images/hammer.png"
         }
     ];  
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+
+    }
+
+    performFilter(filterBy : string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product : IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
   }
